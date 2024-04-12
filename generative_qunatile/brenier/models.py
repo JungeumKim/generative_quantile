c602e7c512d2e3c7248ca11663c9b1e15134e4d5
JungeumKim <snukingdom@gmail.com>
2024-04-12 14:00:12 -0500

    making breiner working in my env.

--- generative_qunatile/brenier/models.py
+++ generative_qunatile/brenier/models.py
@@ -5,7 +5,7 @@ from ot_modules.icnn import *
 #from supp.distribution_output import *
 from supp.piecewise_linear import *
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = "cpu"# torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 attributes = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie', 'Young']
 
 
