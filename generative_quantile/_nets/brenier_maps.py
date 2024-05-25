2522ea9bfa2d128a83874d9f7d072003af91b957
JungeumKim <snukingdom@gmail.com>
2024-05-25 11:10:32 -0500

    Brenier cleaning up done

--- generative_quantile/_nets/brenier_maps.py
+++ generative_quantile/_nets/brenier_maps.py
@@ -1,28 +1,13 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from _nets.icnn import ICNN_LastInp_Quadratic
-#from supp.distribution_output import *
 from _nets.basic_nets import  MLP
 
 from IPython.core.debugger import set_trace
-#device = "cpu"# torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-attributes = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie', 'Young']
 
-
-def dual_JK(U, Y_hat, Y, X, eps=0,efficient=False):
+def dual_JK(U, Y_hat, Y, X):
     alpha, beta = Y_hat # alpha(U) + beta(U)^{T}X
     # alpha: n x 1, beta: n x d, X: n x d,
-
-    #if efficient:
-    #    loss = (alpha.view(-1,1) + (beta.unsqueeze(2) * X.permute(1, 0).unsqueeze(0)).mean(-1).sum(1).view(-1,1)) # n x 1
-    #    #beta.unsqueeze(2): n x d x 1
-    #    #X.permute(1, 0).unsqueeze(0): 1 x d x n
-    #    # their multiplication: n x d x n : mean: nxd
-    #else:
-    #    loss = (alpha.view(-1,1) + (beta * X).sum(1).view(-1,1)) # n x 1
-
-
 
     Y = Y.permute(1, 0) #d x n
     X = X.permute(1, 0) # d x n
@@ -31,27 +16,14 @@ def dual_JK(U, Y_hat, Y, X, eps=0,efficient=False):
 
     # U: n x d  , Y: d x n
     UY = torch.mm(U, Y) # n x n
-    # (U, Y), (U, X), beta.shape(bs, nclass), X.shape(bs, nclass)
-    #print(BX.shape, UY.shape, alpha.shape)
     psi = UY - alpha - BX # n x n - n x 1 - n x n
-
     sup, _ = torch.max(psi, dim=0) # n
 
-    #print(sup.shape)
-    #print(UY.min(), UY.max(), sup.mean())
     #Corrected loss: 2024/May/2
     loss = torch.mean(alpha)
     loss += sup.mean()
 
-
-    if eps == 0:
-        return loss
-
-    l = torch.exp((psi-sup)/eps)
-    loss += eps*torch.mean(l)
     return loss
-
-
 
 
 class BiRNN(nn.Module):
@@ -67,6 +39,9 @@ class BiRNN(nn.Module):
         self.to(device)
 
     def forward(self, x):
+        '''
+        input x: (batch_size x T x dim_x)
+        '''
         # Set initial states
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device) # 2 for bidirection
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
@@ -76,9 +51,8 @@ class BiRNN(nn.Module):
         # Decode the hidden state of the last time step
         out = self.fc(out[:, -1, :])
         if self.bn_last:
-            return self.norm(out), out
+            return self.norm(out)
         return out
-
 
 class DeepSets(nn.Module):
     def __init__(self, dim_x, dim_ss, factor=16, num_layers=2, device="cuda", bn_last=True):
@@ -110,37 +84,33 @@ class DeepSets(nn.Module):
         phi = self.common_feature_net(x.view(-1,shape[-1])).view(x.shape[0],x.shape[1],-1).sum(1)
         out = self.next_net(phi)
         if self.bn_last:
-            return self.norm(out), out
+            return self.norm(out)
         return out
 
-
 class ConditionalConvexQuantile(nn.Module):
-    def __init__(self, xdim, ydim, fdim=0, f2dim=0, a_hid=512, a_layers=3, b_hid=512,
-                 b_layers=1, device="cuda",use_f=True,deepf = False, n_layers=2, factor=16):
+    def __init__(self, xdim, udim,
+                 f1dim=0, f2dim=0, factor=16, f1_layers=2,
+                 a_hid=512, a_layers=3, b_hid=512,b_layers=1,
+                 device="cuda"):
+
         super(ConditionalConvexQuantile, self).__init__()
-        self.xdim = xdim
-        self.fdim = fdim
-        self.f2dim=f2dim
-        self.a_hid=a_hid
-        self.a_layers=a_layers
-        self.b_hid=b_hid
-        self.b_layers=b_layers
-        self.use_f=use_f
+        self.f1dim = f1dim
+        self.f2dim= f2dim
 
-        self.alpha = ICNN_LastInp_Quadratic(input_dim=ydim,
-                                    hidden_dim=self.a_hid,#1024,#512
+        self.alpha = ICNN_LastInp_Quadratic(input_dim=udim,
+                                    hidden_dim=a_hid,
                                     activation='celu',
-                                    num_layer=self.a_layers)
-        self.beta = ICNN_LastInp_Quadratic(input_dim=ydim,
-                                    hidden_dim=self.b_hid,
+                                    num_layer=a_layers)
+        self.beta = ICNN_LastInp_Quadratic(input_dim=udim,
+                                    hidden_dim=b_hid,
                                     activation='celu',
-                                    num_layer=self.b_layers,
-                                    out_dim=self.fdim+self.f2dim)
+                                    num_layer=b_layers,
+                                    out_dim=self.f1dim+self.f2dim)
 
-        if  self.fdim>0:
+        if  self.f1dim>0:
             self.f1 = DeepSets(dim_x=xdim,
-                              dim_ss=self.fdim,
-                              factor=factor, num_layers=n_layers, device=device)
+                              dim_ss=self.f1dim,
+                              factor=factor, num_layers=f1_layers, device=device)
 
         if  self.f2dim>0:
              self.f2 = BiRNN(input_size=xdim,
@@ -150,76 +120,25 @@ class ConditionalConvexQuantile(nn.Module):
 
         self.device =device
         self.to(device)
-        #self.f = ShallowRegressionLSTM(1, 128)
-        # MLP
 
-        #self.bn1 = nn.BatchNorm1d(self.xdim, momentum=1.0, affine=False)
-
-        #self.f = nn.BatchNorm1d(self.xdim, affine=False)
     def f(self, x):
-        if self.fdim>0 and  self.f2dim ==0:
+        if self.f1dim>0 and  self.f2dim ==0:
             return self.f1(x)
-        elif self.fdim==0 and  self.f2dim >0:
+        elif self.f1dim==0 and  self.f2dim >0:
             return self.f2(x)
         else:
-            x1,xv1 = self.f1(x)
-            x2,xv2 = self.f2(x)
-            x, xv = torch.cat([x1,x2], 1), torch.cat([xv1,xv2], 1)
-            return x,xv
+            x= torch.cat([self.f1(x),self.f2(x)], 1)
+            return x
 
-    def forward(self, z, x=None):
-        # we want onehot for categorical and non-ordinal x.
-        if self.xdim == 0:
-            return self.alpha(z)
+    def forward(self, z, x):
         alpha = self.alpha(z)
-        beta = self.beta(z) #torch.bmm(self.beta(z).unsqueeze(1), self.fc_x(x).unsqueeze(-1))
-        #quad = (z.view(z.size(0), -1) ** 2).sum(1, keepdim=True) / 2
-        if x is None:
-            return alpha, beta #, self.fc_x(x)
-        else:
-            return alpha, beta, self.f(x)
+        beta = self.beta(z)
+        f = self.f(x)
+        return alpha, beta, f
     
-    def grad(self, u, x=None, onehot=True):
-        if self.use_f:
-            if onehot and self.xdim > 0:
-                x,xv = self.to_onehot(x,self.xdim)
-            elif x != None:
-                x, xv = self.f(x)#self.bn1(x)
-        else:
-            x,xv=x.squeeze(1),x.squeeze(1)
-        u.requires_grad = True 
-        phi = self.alpha(u).view(-1)
-        if self.xdim != 0 and x != None:
-            #set_trace()
-            #phi += (torch.bmm(self.beta(u).unsqueeze(1), x.unsqueeze(-1)).squeeze(-1)).sum()
-            phi += (self.beta(u) * x).sum(1).view(-1)
-        phi = phi.sum()
+    def grad(self, u, x):
+        f = self.f(x)
+        u.requires_grad = True
+        phi = (self.alpha(u).view(-1) + (self.beta(u) * f).sum(1).view(-1)).sum()
         d_phi = torch.autograd.grad(phi, u, create_graph=True)[0]
-        return d_phi, xv
-
-    
-    def invert(self, y):
-        raise NotImplementedError
-    
-    def to_onehot(self, x,n_c):
-        with torch.no_grad():
-            onehot = torch.zeros((x.shape[0], n_c), device=self.device)
-            onehot.scatter_(dim=-1, index=x.view(x.shape[0], 1), value=1)
-            #onehot -= 1/self.xdim
-            #onehot = self.bn1(onehot)
-
-        #set_trace()
-        if self.use_f:
-            return onehot
-        else:
-            return self.f(onehot.unsqueeze(1))
-
-    def weights_init_uniform_rule(self, m):
-        classname = m.__class__.__name__
-        # for every Linear layer in a model..
-        if classname.find('Linear') != -1:
-            # get the number of the inputs
-            n = m.in_features
-            y = 1.0/np.sqrt(n)
-            m.weight.data.uniform_(-y, y)
-            m.bias.data.fill_(0)
+        return d_phi
