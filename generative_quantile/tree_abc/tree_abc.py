656aaa3e2da460cd88bb73b1aff14bacf8d6511d
JungeumKim <snukingdom@gmail.com>
2024-05-28 09:39:59 -0500

    Tree ABC sampler fixed.

--- generative_quantile/tree_abc/tree_abc.py
+++ generative_quantile/tree_abc/tree_abc.py
@@ -258,16 +258,16 @@ class TreeABC:
     def sampler(self, batch_size=100, n_proposal=5000):
 
         theta_now, _ = self.TP.sample(n_proposal, returnbins=True) #: (n_proposal, dim)
-        discr_now = self.discrepancy(theta_now, self.xobs) # from (n_proposal, dim) to (n_proposal,)
+        #discr_now = self.discrepancy(theta_now, self.xobs) # from (n_proposal, dim) to (n_proposal,)
         q = self.TP.density(theta_now).reshape(-1)
         imports_now = self.prior_density(self.thetas).reshape(-1)  / q # (n_proposal, ),  (n_proposal, ) -> (n_proposal, )
 
         # importance resampling
-        order = discr_now.argsort()
-        thlp = theta_now[order][-batch_size:]
-        wlp = imports_now[order][-batch_size:]
+        #order = discr_now.argsort()
+        thlp = theta_now#[order][-batch_size:]
+        wlp = imports_now#[order][-batch_size:]
         wlp = wlp / np.sum(wlp)
-        plp_inds = np.random.choice(batch_size, batch_size, replace=True, p=wlp)
+        plp_inds = np.random.choice(len(wlp), batch_size,  replace=True, p=wlp)
 
         return thlp[plp_inds]
 
